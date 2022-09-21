c29241867634f173e33f1ed4d7889e00d5c116a9
Muchnik Illia <muchnik.illia.js@gmail.com>
2022-09-21 12:37:34 +0300

    HeaderMenu

--- client/src/componens/Header.jsx
+++ client/src/componens/Header.jsx
@@ -7,63 +7,26 @@ import Typography from '@mui/material/Typography'
 import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
-import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
-import Tooltip from '@mui/material/Tooltip'
 import MenuItem from '@mui/material/MenuItem'
 import AdbIcon from '@mui/icons-material/Adb'
+import HeaderMenu from './HeaderMenu'
 
 const pages = ['Products', 'Pricing']
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = useState(null)
-  const [anchorElUser, setAnchorElUser] = useState(null)
-  const [isAuth, setIsAuth] = useState(false)
 
-  const settings = isAuth
-    ? [
-        {
-          name: 'Profile',
-          handle: () => {
-            alert('Profile')
-          },
-        },
-        {
-          name: 'Logout',
-          handle: () => {
-            alert('Logout')
-          },
-        },
-      ]
-    : [
-        {
-          name: 'Login',
-          handle: () => {
-            alert('Login')
-          },
-        },
-        {
-          name: 'Registration',
-          handle: () => {
-            alert('Registration')
-          },
-        },
-      ]
 
   const handleOpenNavMenu = event => {
     setAnchorElNav(event.currentTarget)
   }
-  const handleOpenUserMenu = event => {
-    setAnchorElUser(event.currentTarget)
-  }
-
+  
   const handleCloseNavMenu = () => {
     setAnchorElNav(null)
   }
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
-  }
+ 
 
   return (
     <AppBar position="static">
@@ -155,40 +118,7 @@ const Header = () => {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map(({name, handle}) => (
-                <MenuItem key={name} onClick={handleCloseUserMenu}>
-                  <Typography
-                    textAlign="center"
-                    onClick={handle}
-                  >
-                    {name}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+         <HeaderMenu/>
         </Toolbar>
       </Container>
     </AppBar>
