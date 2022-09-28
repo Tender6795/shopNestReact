aebc92b9bb8f703d81a8fd0e9cb0bf755e3c82db
Muchnik Illia <muchnik.illia.js@gmail.com>
2022-09-28 13:32:03 +0300

    Fix auth

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
-import {  UserService } from 'src/user/user.service';
+import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs'
 import { User } from 'src/user/entities/user.entity';
 import { FilesService } from 'src/files/files.service';
@@ -24,14 +24,13 @@ export class AuthService {
         if (candidate) {
             throw new HttpException('There is already a user with this email', HttpStatus.BAD_REQUEST)
         }
-        const hashPassword = await bcrypt.hash(userDto.password, 5)
         const fileName = await this.fileService.createFile(image)
-        const user = await this.userService.create({ ...userDto, password: hashPassword, avatar: fileName })
+        const user = await this.userService.create({ ...userDto, avatar: fileName })
         return this.generateToken(user)
     }
 
     private async generateToken(user: User) {
-        const payload = { email: user.email, id: user.id, roles: user.roles }  // нет роли (
+        const payload = { email: user.email, id: user.id, roles: user.roles }
         return {
             token: this.jwtService.sign(payload)
         }
@@ -39,7 +38,6 @@ export class AuthService {
 
     private async validateUser(userDto: CreateUserDto) {
         const user = await this.userService.findOneByEmail(userDto.email)
-        console.log(user)
         const passwordEquals = await bcrypt.compare(userDto.password, user.password)
         if (user && passwordEquals) {
             return user
